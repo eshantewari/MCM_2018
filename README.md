fe9f8d976887d40a8cf5ab3f99e071f9a8b855de
Asher Spector <amspector100@gmail.com>
2018-02-08 22:25:53 -0500

    Added datadic, additional graphing function

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MCM_2018
+
+## Initial Formatting Docs:
+formatinitialdata will structure the data better (it will also split the file into four files, one for each state)
+graphinitialdata creates a data dictionary and graphs specific attributes within and across states
+
+## Understanding MSN Codes (copied from ESA website):
+The MSNs are five-character codes, most of which are structured as follows:
+First and second characters - describes an energy source (for example, NG for natural gas, MG for motor gasoline)
+Third and fourth characters - describes an energy sector or an energy activity (for example, RC for residential consumption, PR for production)
+Fifth character - describes a type of data (for example, P for data in physical unit, B for data in billion Btu)
--- graphinitialdata.py
+++ graphinitialdata.py
@@ -2,22 +2,27 @@ import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 
 # Gets path of data for state
 def get_path(statecode):
     return 'MCM' + str(statecode) + 'Data.csv'
+
+# Create data dictionary, the path should be to a csv of the data dictionary from the competition
+def create_data_dictionary(path):
+    datadic = pd.read_csv(path, header=0, index_col=0)
+    return datadic
 
 # Graph a specific attribute of a list of states over time
 def graph_attribute(attribute, statecodes):
 
     fig, ax = plt.subplots()
 
+    # Get colors
     colors = plt.cm.rainbow(np.linspace(0, 1, len(statecodes)))
 
     # Run through statecodes list
     for color, state in zip(colors, statecodes):
-
-        # Get colors
 
         statedata = pd.read_csv(get_path(state), index_col = 0, header=0)
         ax.plot(statedata.index, statedata.loc[:, attribute], color=color, label=state)
@@ -26,4 +31,30 @@ def graph_attribute(attribute, statecodes):
     ax.legend()
     plt.show()
 
-graph_attribute('CLOCB', ['TX', 'AZ', 'CA', 'NM'])
+
+# Graph multiple attributes of a single state, get correlation between them, presumably they have the same units
+def graph_multiple_attributes(attributes, state):
+
+    fig, ax = plt.subplots()
+
+    # Get colors and state data
+    colors = plt.cm.rainbow(np.linspace(0, 1, len(attributes)))
+    statedata = pd.read_csv(get_path(state), index_col=0, header=0)
+
+    # Run through attributes list
+    for color, attribute in zip(colors, attributes):
+
+        ax.plot(statedata.index, statedata.loc[:, attribute], color=color, label=attribute)
+
+    ax.set(title = 'Attributes in {} Over Time'.format(state), ylabel = 'Use', xlabel='Year')
+    ax.legend()
+    plt.show()
+
+# Run examples of graphing functions and create the data dictionary
+
+datadic = create_data_dictionary('ProblemCDataDic.csv')
+
+print(datadic.columns)
+graph_attribute('BMTCB', ['AZ', 'CA', 'NM', 'TX'])
+attributes = [index for index in datadic.index if 'TCV' in index]
+graph_multiple_attributes(attributes, 'TX')
