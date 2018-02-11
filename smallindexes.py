82e523ad65a7db3ebb8f0e54d1483999e95b45ac
Asher Spector <amspector100@gmail.com>
2018-02-10 21:49:20 -0500

    Added pop adjustment functionality

--- smallindexes.py
+++ smallindexes.py
@@ -13,9 +13,24 @@ sourcedic = {'CL':'Coal', 'NN': 'Natural Gas', 'PA': 'Petroleum Products',
              'WD': 'wood', 'WS': 'Biomass waste', 'ES': 'Electricity Sales',
              'LO': 'Electrical System Losses', 'WY': 'Wind'}
 
+# adjusts for population per capita
+def adjust_for_population(statedata, unit):
+    factors = [factor for factor in statedata.columns if factor[-1] == unit]
+    statedata[factors] = statedata[factors].divide((statedata['TPOPP']*1000), axis=0)
+    return statedata[factors]
+
+statedata, datadic = gd.get_data('TX')
+print(statedata['TPOPP'], statedata['TETXB'])
+statedata = adjust_for_population(statedata, 'B')
+print(statedata['TETXB'])
+
 # Helper function, will either graph a profile of the given sector's energy use in BTU
 # or the given source's energy use in BTU. It does the bulk of the work to graph.
-def graph_either_profile(attributelist, given, statedata, datadic, min, state):
+def graph_either_profile(attributelist, given, statedata, datadic, min, state, pop_adj=True):
+
+    # Adjust for state population
+    if pop_adj == True:
+        statedata = adjust_for_population(statedata, 'B')*1000
 
     # Get rid of factors which are less than the min times the mean of the attributelist
     means = statedata[attributelist].mean()
@@ -31,8 +46,11 @@ def graph_either_profile(attributelist, given, statedata, datadic, min, state):
     fig, ax = plt.subplots()
     fig.set_size_inches(14, 8)
 
-    # Set labels and title, notify the
-    ax.set(xlabel='Year', ylabel='Consumption (Billions BTU)')
+    # Set labels and title, depending on some of the options
+    if pop_adj:
+        ax.set(xlabel='Year', ylabel='Consumption Per Capita (Millions BTU)')
+    else:
+        ax.set(xlabel='Year', ylabel='Consumption (Billions BTU)')
     if given in sourcedic:
         ax.set(title = '{} {} Energy Use, 1960-2009'.format(state, sourcedic[given]))
     elif given in sectordic:
@@ -64,33 +82,33 @@ def graph_either_profile(attributelist, given, statedata, datadic, min, state):
 
 
 # Will graph a profile of the sector's energy use, in BTU
-def graph_sector_profile(sector, statedata, datadic, min, state=''):
+def graph_sector_profile(sector, statedata, datadic, min, state='', pop_adj = True):
 
     # Find the right attributes/factors
     attributelist = [factor for factor in statedata.columns if factor[0:2] in sourcedic and factor[2:4] == sector
                      and factor[-1] == 'B']
 
-    graph_either_profile(attributelist, sector, statedata, datadic, min, state)
+    graph_either_profile(attributelist, sector, statedata, datadic, min, state, pop_adj)
 
 
 
-def graph_source_profile(source, statedata, datadic, min, state=''):
+def graph_source_profile(source, statedata, datadic, min, state='', pop_adj = True):
 
     # Find the right attributes/factors
     attributelist = [factor for factor in statedata.columns if factor[0:2] == source and factor[2:4] in sectordic
                      and factor[-1] == 'B']
 
-    graph_either_profile(attributelist, source, statedata, datadic, min, state)
+    graph_either_profile(attributelist, source, statedata, datadic, min, state, pop_adj)
 
 
 
-def create_sector_profile(state, sector):
+def create_sector_profile(state, sector, pop_adj = True):
     statedata, datadic = gd.get_data(state)
-    graph_sector_profile(sector, statedata, datadic, 0.01, state)
+    graph_sector_profile(sector, statedata, datadic, 0.01, state, pop_adj = True)
 
 def create_source_profile(state, source):
     statedata, datadic = gd.get_data(state)
-    graph_source_profile(source, statedata, datadic, 0.01, state)
+    graph_source_profile(source, statedata, datadic, 0.01, state, pop_adj = True)
 
 
 create_sector_profile('AZ', 'CC')
